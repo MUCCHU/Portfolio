a09a1e09cb6e7bf3b5f3bbb69dddb1bbe06038ab
Harsh Wasnik <harshwasnik160602@gmail.com>
2023-10-29 17:18:32 +0530

    updated loader+icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Harsh Wasnik',
     description:
       'Harsh Wasnik is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://harshwasnik.tech', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@HarshWasnik1606',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

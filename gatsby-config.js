dc83d55740b32f1ced53e70f53ddbb60d8e75fcf
Harsh Wasnik <harshwasnik160602@gmail.com>
2023-11-22 10:47:41 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,10 +150,25 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-45666519-2',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-WVHHSW6VVL', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
+
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: 'G-8P5CR0RELQ',
+    //   },
+    // },
   ],
 };

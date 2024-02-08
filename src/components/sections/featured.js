42ff280b17613fd58152fdd3cccae18f621e981c
MUCCHU <harshwasnik160602@gmail.com>
2024-02-08 10:02:27 +0530

    removed green effect from images

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -290,8 +290,6 @@ const StyledProject = styled.li`
 
     .img {
       border-radius: var(--border-radius);
-      mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1) brightness(90%);
 
       @media (max-width: 768px) {
         object-fit: cover;
